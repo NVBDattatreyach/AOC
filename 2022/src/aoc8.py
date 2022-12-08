e22432d39f13c728c0d1830d4f57db7b7c8e6282
NVBDattatreyach <70585834+NVBDattatreyach@users.noreply.github.com>
2022-12-08 16:33:44 +0530

    corrected path

--- 2022/src/aoc8.py
+++ 2022/src/aoc8.py
@@ -1,4 +1,7 @@
-with open("input.txt","r") as f:
+import os
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(HERE,"../input/input_8.txt"),"r") as f:
     input=[x.strip("\n") for x in f.readlines()]
     rows,columns=len(input),len(input[0])
     visible_count=0
@@ -19,7 +22,6 @@ with open("input.txt","r") as f:
                 line=new_line
                 input[i]=line
     max_bottom=[int(x) for x in reversed(input[-1])]
-    print(visible_count)
     for i,line in enumerate(reversed(input[1:-1]),1):
         max_right=int(line[-1])
         for j,c in enumerate(reversed(line[1:-1]),1):
