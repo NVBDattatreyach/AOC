1d0ff4089e70389b9efdc85ec625eb99fcf7f5c4
NVBDattatreyach <70585834+NVBDattatreyach@users.noreply.github.com>
2022-12-09 00:37:47 +0530

    solution part 2

--- 2022/src/aoc8.py
+++ 2022/src/aoc8.py
@@ -1,50 +1,49 @@
 import os
 
+bottom_to_original=lambda a,b,c:(c-b-1,a)    
+original = lambda a,b,c:(a,b)
+top_to_original=lambda a,b,c:(b,a)
+right_to_original=lambda a,b,c:(a,c-1-b)
+
+def score(s,a,f):
+    global score_arr
+    c=len(s)
+    st=[0]
+    for i,ch in enumerate(s[1:-1],1):
+        while(len(st)>0):
+            if(int(ch)>int(s[st[-1]])):
+                st.pop()
+            else:
+                x,y=f(a,i,c)
+                score_arr[x][y]*=i-st[-1]
+                break
+        if(len(st)==0):
+            x,y=f(a,i,c)
+            score_arr[x][y]*=i
+        st.append(i)
+    
+def traverse(l,f):
+    r=len(l)
+    c=len(l[0])
+    for a in range(r):
+        s=""
+        for b in range(c): 
+            x,y=f(a,b,c)
+            ch=l[x][y]
+            s+=ch
+        score(s,a,f)
+
 HERE = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(HERE,"../input/input_8.txt"),"r") as f:
     input=[x.strip("\n") for x in f.readlines()]
-    rows,columns=len(input),len(input[0])
-    visible_count=0
-    location=set()
-    max_top=[int(x) for x in input[0]]
-    for i,line in enumerate(input[1:-1],1):
-        max_left=int(line[0])
-        for j,c in enumerate(line[1:-1],1):
-            visible=False
-            if(int(c)>max_top[j]):
-                visible=True
-                max_top[j]=int(c)
-            if(int(c)>max_left):
-                visible=True
-                max_left=int(c)
-            if(visible):
-                location.add((i,j))
-    max_bottom=[int(x) for x in reversed(input[-1])]
-    for i,line in enumerate(reversed(input[1:-1]),1):
-        max_right=int(line[-1])
-        for j,c in enumerate(reversed(line[1:-1]),1):
-            visible=False
-            if(int(c)>max_bottom[j]):
-                visible=True
-                max_bottom[j]=int(c)
-            if(int(c)>max_right):
-                visible=True
-                max_right=int(c)
-            if(visible):
-                location.add((rows-1-i,columns-1-j))
-left_visible=0
-right_visible=0
-top_visible=0
-bottom_visible=0
-for i in range(rows):
-    if(input[i][0]!='0'):
-        left_visible+=1
-    if(input[i][-1]!='0'):
-        right_visible+=1
-for i in range(1,columns-1):
-    if(input[0][i]!='0'):
-        top_visible+=1
-    if(input[-1][i]!='0'):
-        bottom_visible+=1
-print(len(location)+left_visible+right_visible+top_visible+bottom_visible)
-print(len(location)+2*rows+2*(columns-2)) 
+    rows,cols=len(input),len(input[0])
+    score_arr=[[1 if (1<=j<cols-1) else 0 for j in range(cols)] if (1<=i<rows-1)
+        else [0 for j in range(cols)] for i in range(rows)
+    ]
+    traverse(input,bottom_to_original)
+    traverse(input,right_to_original)
+    traverse(input,top_to_original)
+    traverse(input,original)
+    ans=max([ max(x) for x in score_arr])
+    print(ans)
+
