e032d8c1c669f811f5d76abc04b7466972a8fce8
NVBDattatreyach <70585834+NVBDattatreyach@users.noreply.github.com>
2022-12-09 20:52:36 +0530

    solution part 2

--- 2022/src/aoc9.py
+++ 2022/src/aoc9.py
@@ -1,61 +1,84 @@
 import os
+import time
 
-
-class Position:
+class Knot:
     def __init__(self):
         self.x=0
         self.y=0
     def up(self):
-        self.x+=1
-    def down(self):
-        self.x-=1
-    def left(self):
-        self.y-=1
-    def right(self):
         self.y+=1
+    def down(self):
+        self.y-=1
+    def left(self):
+        self.x-=1
+    def right(self):
+        self.x+=1
     def __str__(self):
         return "x:{} y:{}".format(self.x,self.y)
     def move_adjacent(self,other):
-        if(self.x-other.x>1):
+        if(self.y-other.y>1):
             self.down()
-            self.y=other.y
-        elif(self.x-other.x<-1):
-            self.up()
-            self.y=other.y
-        elif(self.y-other.y>1):
-            self.left()
-            self.x=other.x
+            if(self.x>other.x):
+                self.left()
+            elif(self.x<other.x):
+                self.right()
         elif(self.y-other.y<-1):
+            self.up()
+            if(self.x>other.x):
+                self.left()
+            elif(self.x<other.x):
+                self.right()
+        elif(self.x-other.x>1):
+            self.left()
+            if(self.y>other.y):
+                self.down()
+            elif(self.y<other.y):
+                self.up()
+        elif(self.x-other.x<-1):
             self.right()
-            self.x=other.x
-        else:
-            pass
+            if(self.y>other.y):
+                self.down()
+            elif(self.y<other.y):
+                self.up()
     def is_adjacent(self,other):
         return max(abs(self.x-other.x),abs(self.y-other.y))<=1
 
 HERE=os.path.dirname(os.path.abspath(__file__))
 
-head=Position()
-tail=Position()
+knots=[Knot() for _ in range(10)]
 tail_position=set()
 with open(os.path.join(HERE,"../input/input_9.txt"),"r") as f:
     input=[x.strip("\n") for x in f.readlines()]
+    max_x=0
+    min_x=0
+    max_y=0
+    min_y=0
     for line in input:
         move,steps=line.split(" ")
         print("== {} ==".format(line))
         for j in range(int(steps)):
             if(move=='U'):
-                head.up()
+                knots[0].up()
             elif(move=='D'):
-                head.down()
+                knots[0].down()
             elif(move=='R'):
-                head.right()
+                knots[0].right()
             else:
-                head.left()
-            tail.move_adjacent(head)
-            print("head :{}\ttail :{}".format(head,tail))
-            tail_position.add((tail.x,tail.y))
-print(len(tail_position))
+                knots[0].left()
+            max_x=max(max_x,knots[0].x)
+            min_x=min(min_x,knots[0].x)
+            max_y=max(max_y,knots[0].y)
+            min_y=min(min_y,knots[0].y)
+            for i in range(1,10):
+                knots[i].move_adjacent(knots[i-1])
+                max_x=max(max_x,knots[i].x)
+                min_x=min(min_x,knots[i].x)
+                max_y=max(max_y,knots[i].y)
+                min_y=min(min_y,knots[i].y)
+                
+            tail_position.add((knots[-1].x,knots[-1].y))
+            
+print(len(tail_position),min_x,min_y,max_x,max_y)
                 
         
 
